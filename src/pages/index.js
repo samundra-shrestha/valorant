c69deb254551317aa210a05ce4fadaaaba74832f
samundra shrestha <samundras41@gmail.com>
2023-12-16 00:52:08 +0545

    project setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@components/styles/Home.module.css'
+// import styles from '@components/styles/Home.module.scss'
+import largeImage from '@images/large-logo.svg';
+import heroImage from '@images/hero__image.png';
+import { useEffect, useRef } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const largeLogoRef = useRef();
+  const heroImageRef = useRef();
+  const backgroundRef = useRef();
+  useEffect(() => {
+    const logo = largeLogoRef.current;
+    const hero = heroImageRef.current;
+    const bg = backgroundRef.current;
+
+    logo.style.scale="1";
+    hero.style.scale=".92";
+    setTimeout(function () {
+      bg.style.setProperty("--bg-color", "var(--red)")
+    }, 150)
+    console.log(logo, hero)
+  })
   return (
     <>
       <Head>
@@ -14,99 +30,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+      <main ref={backgroundRef}>
+        <h1 title="Valorant">Valorant</h1>
+        <div className="large__logo" ref={largeLogoRef}>
+          <Image src={largeImage} alt='asd' />
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className="hero__image " ref={heroImageRef}>
+          <Image src={heroImage} alt='asd' />
         </div>
       </main>
     </>
